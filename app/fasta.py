5eb9e572fc88039a9c84d9601768a2b583792a72
Mihai Todor <todormihai@gmail.com>
2023-08-31 22:38:01 +0100

    Try to download FASTA files and FASTA index files from GitHub
    This might prevent the memory spike when creating the index files
    from scratch.

--- app/fasta.py
+++ app/fasta.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from urllib.request import urlretrieve
-from utilities.SPDI_Normalization import init_fasta
+
+FASTA_INDEX_RELEASE_TAG = '113c119'
 
 
 def download_fasta():
@@ -12,11 +13,16 @@ def download_fasta():
             filename = build + '_latest_genomic.fna.gz'
             filepath = 'FASTA/' + filename
 
-            # Download files
+            # Download FASTA files
             if not Path(filepath).is_file():
-                urlretrieve('https://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/' + build + '_latest/refseq_identifiers/' + filename, filepath)
+                urlretrieve('https://github.com/FHIR/genomics-operations/releases/download/' + FASTA_INDEX_RELEASE_TAG + '/' + filename, filepath)
 
-        # Build indexes
-        init_fasta()
+            indexFilename = filename + '.fxi'
+            indexFilepath = 'FASTA/' + indexFilename
+
+            # Download FASTA index files
+            if not Path(indexFilepath).is_file():
+                urlretrieve('https://github.com/FHIR/genomics-operations/releases/download/' + FASTA_INDEX_RELEASE_TAG + '/' + indexFilename, indexFilepath)
+
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
