db5eafa2af75a1c15a546aa1e1936847e4b5d0e1
Mihai Todor <todormihai@gmail.com>
2024-01-06 12:46:27 +0000

    Disable HGVS strict bounds checks
    This is a workaround for some liftover failures: https://github.com/biocommons/hgvs/issues/717

--- tests/integration_tests/test_normalize_variant_utility.py
+++ tests/integration_tests/test_normalize_variant_utility.py
@@ -12,3 +12,15 @@ def test_normalize_variant_utility(client):
     response = client.get(url)
 
     assert response.status_code == 200
+
+
+def test_normalize_variant_utility_transcript_ref_disagree_position_variant(client):
+    """
+    This is a specific corner case where liftover fails if HGVS strict bounds checks are enforced
+    Details here: https://github.com/biocommons/hgvs/issues/717
+    """
+    url = tu.normalize_variant_query('variant=NC_000001.10:g.145592073A>T')
+
+    response = client.get(url)
+
+    assert response.status_code == 200
